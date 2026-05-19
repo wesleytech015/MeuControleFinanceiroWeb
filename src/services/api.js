e5dbe91ca6a3a5d4a5d99ebc7c8c7a2273a0a6f7
Eliézer Costa <eliezercosta.ti@gmail.com>
2026-05-19 19:38:28 -0300

    resolve conflito da api.js entre feature e develop

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "http://localhost:3000/api"
 });
 
+feature/estilizacao-frontend
 export default api;
 
 //teste para contribuidor
+=======
+export async function listarTransacoes() {
+  const resposta = await api.get("/transacoes");
+  return resposta.data;
+}
+
+export async function criarTransacao(transacao) {
+  const resposta = await api.post("/transacoes", transacao);
+  return resposta.data;
+}
+
+export async function deletarTransacao(id) {
+  const resposta = await api.delete(`/transacoes/${id}`);
+  return resposta.data;
+}
+
+export default api;
